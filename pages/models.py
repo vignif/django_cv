0f8b21c469af881fb067adb8b0d87505151ec8cc
francesco <vignif@gmail.com>
2021-04-28 11:24:10 +0200

    initial setup schema

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.forms import ModelForm
+import datetime
 
-# Create your models here.
+
+class UserInfo(models.Model):
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    bd_date = models.DateField(("date of birth"), default=datetime.date.today)
+    phone = models.CharField(max_length=18)  # todo validators
+
+    def __str__(self):
+        return self.name
+
+
+class Employer(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    date_founding = models.DateField(("founding date"), default=datetime.date.today)
+    url = models.CharField(max_length=18)  # todo validators
+
+    def __str__(self):
+        return self.name
+
+
+class WorkExperience(models.Model):
+    employee = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
+    employer = models.ForeignKey(Employer, on_delete=models.CASCADE)
+    date_start = models.DateField(
+        ("Initial date of employment"), default=datetime.date.today
+    )
+    date_end = models.DateField(
+        ("Finel date of employment"), default=datetime.date.today
+    )
+
+    def __str__(self):
+        return self.employee.name + " worked at " + self.employer.name
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    date_founding = models.DateField(("founding date"), default=datetime.date.today)
+    url = models.CharField(max_length=18)  # todo validators
+
+    def __str__(self):
+        return self.name
+
+
+class Education(models.Model):
+    employee = models.ForeignKey(UserInfo, null=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.employee.name + " studied at " + self.name
+
+
+# FORMS
+
+
+class UserInfoForm(ModelForm):
+    class Meta:
+        model = UserInfo
+        fields = ["name", "surname", "bd_date", "phone"]
--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, CvPageView, AccountPageView, insertUser
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
-    path('about/', AboutPageView.as_view(), name='about'),
+    path("", HomePageView.as_view(), name="home"),
+    path("about/", AboutPageView.as_view(), name="about"),
+    path("cv/", CvPageView.as_view(), name="cv"),
+    path("account/", insertUser, name="account"),
 ]
